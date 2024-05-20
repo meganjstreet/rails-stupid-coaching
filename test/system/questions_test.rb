f259580b925975693e6170db760247ab920c25f8
Megan Street <meganjstreet@gmail.com>
2024-05-20 16:05:25 +0200

    Adding tests

---
+++ test/system/questions_test.rb
@@ -0,0 +1,25 @@
+require "application_system_test_case"
+
+class QuestionsTest < ApplicationSystemTestCase
+  test "visiting /ask renders the form" do
+    visit ask_url
+    assert_selector "p", text: "Ask your coach anything"
+  end
+
+  test "saying Hello yields a grumpy response from the coach" do
+    visit ask_url
+    fill_in "question", with: "Hello"
+    click_on "Ask"
+    take_screenshot
+
+    assert_text "I don't care, get dressed and go to work!"
+  end
+
+  test "asking Why? yields an angry response from the coach" do
+    visit ask_url
+    fill_in "question", with: "Why?"
+    click_on "Ask"
+    take_screenshot
+    assert_text "Silly question, get dressed and go to work!"
+  end
+end
